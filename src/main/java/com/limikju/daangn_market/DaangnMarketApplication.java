5f885d24676f42c205539edc06c3d3ce94323e18
IkJuLim <limik1004@gmail.com>
2024-10-09 17:44:46 +0900

    프로젝트 생성

---
+++ src/main/java/com/limikju/daangn_market/DaangnMarketApplication.java
@@ -0,0 +1,13 @@
+package com.limikju.daangn_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limikju/daangn_market/DaangnMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limikju.daangn_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
